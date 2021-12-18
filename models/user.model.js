a28ea2b7e22fd73db3beed3df19a079a24101d53
Ammar Sohail <muhammadammarsohail@gmail.com>
2021-12-18 07:51:55 +0500

    Modified: User model

--- models/user.model.js
+++ models/user.model.js
@@ -5,6 +5,10 @@ const userSchema = mongoose.Schema({
     type: String,
     require: true,
   },
+  username: {
+    type: String,
+    require: true,
+  },
   password: {
     type: String,
     require: true,
@@ -13,10 +17,35 @@ const userSchema = mongoose.Schema({
     type: String,
     require: true,
   },
-  username: {
+  name: {
     type: String,
     require: true,
   },
+  age: {
+    type: Number,
+    require: false
+  },
+  country: {
+    type: String,
+    require: false,
+  },
+  city: {
+    type: String,
+    require: false,
+  },
+  roomLink: {
+    type: String,
+    require: false,
+  },
+  trainingClasses: {
+    type: Boolean,
+    require: false,
+  },
+  Courses: {
+    type: [int],
+    require: false,
+  }
+  
 });
 
 module.exports = mongoose.model("users", userSchema, "users");
